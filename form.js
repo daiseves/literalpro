25cc2fa2c4e4fceb0c7f44270774195bbdb6ab0c
Lariña <33741424+daiseves@users.noreply.github.com>
2022-05-30 08:46:48 -0300

    Update form.js

--- form.js
+++ form.js
@@ -8,6 +8,11 @@ function validate() {
   alert( "Em.ail inválido" );
   document.form.email.focus() ;
   return false;
+  
+ var email = $("#email").val();
+ if ((/(.+)@(.+){2,}\.(.+){2,}/.test(email)) || email=="" || email==null) { } else {
+  alert( "Email inválido" );
+  return false;
  }
  if( document.form.message.value == == "" ) {
   alert( "Por favor, ingrese un mensaje" );
